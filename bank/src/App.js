45d934aa570fd03a7dfbe34fc4fe243326d5c694
rohitsharma <rohitashoksharma19@gmail.com>
2022-12-22 17:51:23 +0530

    new commit

---
+++ bank/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './component/Main';
+import Table from './component/Table';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Main></Main>
+      <Table></Table>
+    </div>
+    
+
+  );
+}
+
+export default App;
