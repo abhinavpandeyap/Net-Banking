2465f5a3d0c1d64cdba33b9746e8c7e1fe3086cb
abhinavpandeyap <abhinavpandeyri2@gmail.com>
2022-12-24 17:32:41 +0530

    sign up added

---
+++ netbankingapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './Signup';
+
+function App() {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
